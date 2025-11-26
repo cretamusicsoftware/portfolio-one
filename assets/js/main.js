61e4b4b269e60b5345ec1e3fd92e03c57354639d
mike-creta <cretamusickenya@gmail.com>
2025-11-26 02:56:11 -0800

    home done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,26 @@
-/*=============== HOME SPLIT TEXT ===============*/
+import { animate, splitText, stagger } from "animejs";
 
+const { animate } = anime;
 
-/*=============== SWIPER PROJECTS ===============*/
+const { chars: char1 } = splitText(".home__profession-1", {
+  chars: true,
+});
+const { chars: char2 } = splitText(".home__profession-2", {
+  chars: true,
+});
 
+animate(chars1, {
+  y: [{ to: ["100%", "0%"] }, { to: "-100%", delay: 4000, ease: "in(3)" }],
+  duration: 900,
+  ease: "out(3)",
+  delay: stagger(80),
+  loop: true,
+});
 
-/*=============== WORK TABS ===============*/
-
-
-/*=============== SERVICES ACCORDION ===============*/
-
-
-/*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/
-
-
-/*=============== COPY EMAIL IN CONTACT ===============*/
-
-
-/*=============== CURRENT YEAR OF THE FOOTER ===============*/ 
-
-
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
-
-/*=============== CUSTOM CURSOR ===============*/
-
-
-/* Hide custom cursor on links */
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+animate(chars2, {
+  y: [{ to: ["100%", "0%"] }, { to: "-100%", delay: 4000, ease: "in(3)" }],
+  duration: 900,
+  ease: "out(3)",
+  delay: stagger(80),
+  loop: true,
+});
